68169ba3a0da298c4c7c3acf0a13456c5c9cdc57
Kevn Roberts <kev@thebrowntrout.net>
2013-10-13 20:12:43 -0600

    adding code to pass tests, and adding another test.

--- numberToDollars.js
+++ numberToDollars.js
@@ -1,7 +1,120 @@
 /* exercise 1 - number to dollars */
 
-function numberToDollars(num){
-    if(typeof num !== 'number'){
-        throw new Error('"num" is not a number');
+var numberToDollars = (function(){
+    'use strict';
+    function stringifySingle(single){
+        var singles = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+        var num = parseInt(single);
+        return singles[num];
     }
-}
+
+    function stringifyTens(ten){
+        var teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+        var tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+        var isTeen = false;
+        var numTen;
+        var tenNum, single;
+        var result = '';
+
+        if(ten.length > 1){
+            // get ten value
+            tenNum = parseInt(ten.charAt(0));
+            if(tenNum === 1){
+                isTeen = true;
+                // get teen value
+                result = teens[parseInt(ten.charAt(1))];
+            } else if(tenNum > 1){
+                // get tens value
+                result = tens[tenNum - 2];
+            }
+        }
+
+        if(!isTeen){
+            single = stringifySingle(ten.charAt(1));
+            if(single !== 'zero'){
+                result += '-' + single;
+            }
+        }
+
+        return result;
+    }
+
+    function stringifyHundreds(numString){
+        var result = '';
+        var single;
+
+        if(numString.length === 3 && numString.charAt(0) !== '0'){
+            result += stringifySingle(numString.charAt(0)) + ' hundred';
+        }
+
+        if(numString.length > 1 && numString.charAt(numString.length - 2) !== '0'){
+            if(result){ result += ' '; }
+            result += stringifyTens(numString.substring(numString.length - 2));
+        } else {
+            single = stringifySingle(numString);
+            if(result && single !== 'zero') {
+                result += ' ' + single;
+            } else if(!result) {
+                result = single;
+            }
+        }
+
+        return result;
+    }
+
+    function numberToDollars(num){
+        var numString, numParts, whole, decimal;
+        var largeGroups = ['thousand', 'million', 'billion', 'trillion', 'quadrillion', 'quintillion'];
+        var wholeGroups = [];
+        var format = '{whole} and {decimal}/100 dollars';
+        var wholeString = '';
+        var i, len; // for iterators
+        var groupResult;
+
+        if(typeof num !== 'number'){
+            throw new Error('"num" is not a number');
+        }
+
+        numString = num.toString();
+        numParts = numString.split('.');
+        whole = numParts[0];
+        decimal = numParts.length === 2 ? numParts[1] : '00';
+
+        // standarize the decimal portion
+        if(decimal.length > 2){
+            decimal = decimal.substring(0, 2);
+        }
+
+        if(decimal.length < 2){
+            decimal = decimal + '0';
+        }
+
+        // whole numbers
+        // break into groups of 3
+        for(i = Math.ceil(whole.length / 3); i > 0; i -= 1){
+            if(whole.length > 3){
+                wholeGroups.push(whole.substring(whole.length - 3));
+                whole = whole.substring(0, whole.length - 3);
+            } else {
+                wholeGroups.push(whole);
+            }
+        }
+
+        // iterate over groups of 3
+        for(i = 0, len = wholeGroups.length; i < len; i += 1){
+            groupResult = stringifyHundreds(wholeGroups[i]);
+            if(i){
+                groupResult += ' ' + largeGroups[i - 1];
+            }
+
+            if(wholeString){ wholeString = ' ' + wholeString; }
+            wholeString = groupResult + wholeString;
+        } 
+
+        wholeString = wholeString.charAt(0).toUpperCase() + wholeString.slice(1);
+
+        return format.replace(/{whole}/, wholeString).replace(/{decimal}/, decimal);
+    }
+
+    return numberToDollars;
+}());
--- tests/numberToDollarsSpec.js
+++ tests/numberToDollarsSpec.js
@@ -17,30 +17,35 @@ describe('numberToDollars', function() {
     it('should return "Twenty-six and 01/100 dollars" when passing 26.01', function(){
         var result = numberToDollars(26.01);
         expect(result).toEqual('Twenty-six and 01/100 dollars');
-    })
+    });
 
     it('should return "Five hundred thirty-two and 50/100 dollars" when passing 532.50', function(){
         var result = numberToDollars(532.50);
         expect(result).toEqual('Five hundred thirty-two and 50/100 dollars');
-    })
+    });
 
     it('should return "Two thousand five hundred twenty-three and 04/100 dollars" when passing 2523.04', function(){
         var result = numberToDollars(2523.04);
         expect(result).toEqual('Two thousand five hundred twenty-three and 04/100 dollars');
-    })
+    });
 
     it('should return "Thirty thousand eight and 00/100 dollars" when passing 30008.00', function(){
         var result = numberToDollars(30008.00);
         expect(result).toEqual('Thirty thousand eight and 00/100 dollars');
-    })
+    });
 
     it('should return "Nine hundred eighty-seven thousand six hundred fifty-four and 32/100 dollars" when passing 987654.32', function(){
         var result = numberToDollars(987654.32);
         expect(result).toEqual('Nine hundred eighty-seven thousand six hundred fifty-four and 32/100 dollars');
-    })
+    });
 
     it('should return "Zero and 01/100 dollars" when passing 0.01', function(){
         var result = numberToDollars(0.01);
         expect(result).toEqual('Zero and 01/100 dollars');
-    })
+    });
+
+    it('should return "One billion two hundred thirty-four million five hundred sixty-seven thousand eight hundred ninety and 00/100 dollars" when passing 1234567890', function(){
+        var result = numberToDollars(1234567890);
+        expect(result).toEqual('One billion two hundred thirty-four million five hundred sixty-seven thousand eight hundred ninety and 00/100 dollars');
+    });
 });
